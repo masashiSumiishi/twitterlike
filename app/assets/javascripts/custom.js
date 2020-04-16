18f47907700016cd61b1fc97e38a6cf355804e22
Masashi <t15519ms@gmail.com>
2020-04-16 15:28:29 +0900

    home pageの作成

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,14 @@
+$(function(){
+  setTimeout(function(){
+    $("#js-mail-left").animate({top:"450px"},1000).animate({left:"150px"},1000, function(){
+      $(this).addClass('shadow');
+    });
+    $("#js-mail-right").animate({top:"450px"},2000).animate({right:"140px"},1000, function(){
+      $(this).addClass('shadow');
+    });
+  }, 1000);
+
+  setTimeout(function(){
+    $('.home__descriptionMessage').fadeIn(1000);
+  }, 5000);
+});
