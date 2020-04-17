9c09f954c688695c593c69667cc568bbb99ca078
Masashi <t15519ms@gmail.com>
2020-04-17 17:44:42 +0900

    add: sessions controller, login機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/help', to:'static_pages#help'
-  get '/about', to:'static_pages#about'
+  get '/help',   to:'static_pages#help'
+  get '/about',  to:'static_pages#about'
   get '/signup', to:'users#new'
+  get '/login',  to:'sessions#new'
+  get '/login',  to:'sessions#create'
+  get '/logout', to:'sessions#destroy'
   resources :users
 end
