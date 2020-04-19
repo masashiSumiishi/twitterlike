e0c45b1688cc05c9a44a8452d5d6cdb702828ee1
Masashi <t15519ms@gmail.com>
2020-04-19 12:36:28 +0900

    add: ダミーのユーザー、testの追加

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "index including pagination" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    assert_select 'a.usersList__link'
+  end
+end
