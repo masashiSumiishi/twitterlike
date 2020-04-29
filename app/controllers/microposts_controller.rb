409995385b76fe7a69847d0b78acd62c0398a4ff
Masashi <t15519ms@gmail.com>
2020-04-29 18:09:12 +0900

    add: micropost/show

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,6 +6,10 @@ class MicropostsController < ApplicationController
     @microposts = Micropost.all
   end
 
+  def show
+    @micropost = Micropost.find(params[:id])
+  end
+
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
